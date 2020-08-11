be33442874409973dd5e5fcee52608730e574c04
adam181189 <adam181189@gmail.com>
2020-08-11 11:53:20 +0000

    layout bug fixes

--- assets/scripts.js
+++ assets/scripts.js
@@ -29,47 +29,48 @@
             var markerCluster = new MarkerClusterer(map, markers,
             {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
       }
+  
 function userInformationHTML(user) {
     return `
         <h2>${user.name}
             <span class="small-name">
-                (@<a href="${user.html_url}" target="_blank">${user.login}
-                </a>
+                (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
             </span>
         </h2>
         <div class="gh-content">
             <div class="gh-avatar">
                 <a href="${user.html_url}" target="_blank">
-                    >img src="${user.avatar_url}" width="80 height="80" alt="${user.login}" />
+                    <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
                 </a>
             </div>
-            </p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
-        </div>
-    `;
+            <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
+        </div>`;
 }
 
 function repoInformationHTML(repos) {
-    if (repos.length === 0) {
+    if (repos.length == 0) {
         return `<div class="clearfix repo-list">No repos!</div>`;
     }
-    
-    var listItemsHTML = repos.map(function (repo) {
+
+    var listItemsHTML = repos.map(function(repo) {
         return `<li>
                     <a href="${repo.html_url}" target="_blank">${repo.name}</a>
-                </li>`
+                </li>`;
     });
 
     return `<div class="clearfix repo-list">
-        <p>
-            <strong>Repo List:</strong>
-        </p>
-        <ul>
-            ${listItemsHTML.join("/n")}
-        </ul>
-    </div>`
+                <p>
+                    <strong>Repo List:</strong>
+                </p>
+                <ul>
+                    ${listItemsHTML.join("\n")}
+                </ul>
+            </div>`;
 }
-      
+
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
 
     var username = $("#gh-username").val();
     if (!username) {
@@ -79,12 +80,10 @@ function fetchGitHubInformation(event) {
 
     $("#gh-user-data").html(
         `<div id="loader">
-            <img src="images/loader.gif" alt="loading..." />
+            <img src="assets/css/loader.gif" alt="loading..." />
         </div>`);
 
-        
-
- $.when(
+    $.when(
         $.getJSON(`https://api.github.com/users/${username}`),
         $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then(
@@ -93,7 +92,7 @@ function fetchGitHubInformation(event) {
             var repoData = secondResponse[0];
             $("#gh-user-data").html(userInformationHTML(userData));
             $("#gh-repo-data").html(repoInformationHTML(repoData));
-            },
+        },
         function(errorResponse) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(
@@ -105,5 +104,5 @@ function fetchGitHubInformation(event) {
             }
         });
 }
-            
-            
+
+$(document).ready(fetchGitHubInformation);
